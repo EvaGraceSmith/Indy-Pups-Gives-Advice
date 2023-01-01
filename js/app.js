2b8bfade4b057fa8b41de2ca27f8688c6587e709
EvaGraceSmith <smithevagrace@gmail.com>
2023-01-01 01:06:29 -0600

    added sound to submit

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 'use strict';
 console.log ('Hello from the js file');
+
+function playDogSoundForSubmit(event){
+  event.preventDefault;
+  let ding = new Audio('../dog-talking-or-howling-72517.mp3');
+  ding.play();
+  alert ('Your question had been submitted.');
+}
+
